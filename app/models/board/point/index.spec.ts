dc469d6b4b87e72ae8cf5bfc2f61c6c6d185c946
Peter Popov <p.dim.popov@gmail.com>
2022-10-30 14:59:04 +0200

    add error handling for constructing board point

--- app/models/board/point/index.spec.ts
+++ app/models/board/point/index.spec.ts
@@ -2,10 +2,13 @@ import { BoardPointOps } from '~/models/board/point';
 import { PointOps } from '~/models/point';
 
 describe('Board Point Operations', () => {
+  const point = PointOps.new(1, 2);
+
   describe(BoardPointOps.new.name, () => {
     it('should create a board point from point', () => {
-      const point = PointOps.new(1, 2);
-      const boardPoint = BoardPointOps.new(point);
+      const boardPoint = BoardPointOps.new(point, [
+        { ...point, x: point.x + 1 },
+      ]);
 
       expect(boardPoint.x).toEqual(point.x);
       expect(boardPoint.y).toEqual(point.y);
@@ -17,6 +20,18 @@ describe('Board Point Operations', () => {
       const boardPoint = BoardPointOps.new(PointOps.new(1, 2), allowedNext);
 
       expect(boardPoint.allowedNext).toEqual(allowedNext);
+    });
+
+    it('should throw when no next points are supplied', () => {
+      const badConstruct = () => BoardPointOps.new(point, []);
+
+      expect(badConstruct).toThrow();
+    });
+
+    it('should throw when no only next point is the same point', () => {
+      const badConstruct = () => BoardPointOps.new(point, [point]);
+
+      expect(badConstruct).toThrow();
     });
   });
 });
--- app/models/board/point/index.ts
+++ app/models/board/point/index.ts
@@ -1,15 +1,37 @@
 import type { Point } from '~/models/point';
 import { PointOps } from '~/models/point';
+import { createMirrorMap } from '~/utils';
 
 export type BoardPoint = Point & {
   id: string
-  allowedNext?: Point[]
+  allowedNext: Point[]
 };
 
-export const BoardPointOps = {
-  new: (point: Point, allowedNext?: Point[]): BoardPoint => ({
+const newBoardPoint = (point: Point, allowedNext: Point[]): BoardPoint => {
+  if (!allowedNext.length) {
+    throw new BoardPointOps.ConstructError('NO_NEXT_POINTS');
+  }
+
+  if (allowedNext.some(PointOps.equals(point))) {
+    throw new BoardPointOps.ConstructError('CURRENT_EXISTS_AS_NEXT');
+  }
+
+  return {
     ...point,
     id: PointOps.serialize(point),
     allowedNext,
-  }),
+  };
+};
+
+class BoardPointConstructError extends Error {
+  static Type = createMirrorMap(['NO_NEXT_POINTS', 'CURRENT_EXISTS_AS_NEXT']);
+
+  constructor(public type: keyof typeof BoardPointOps.ConstructError.Type) {
+    super(type);
+  }
+}
+
+export const BoardPointOps = {
+  new: newBoardPoint,
+  ConstructError: BoardPointConstructError,
 };
