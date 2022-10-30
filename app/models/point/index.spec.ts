8d9674da602ba8b67bdd3bf9105d74a5def9ba6d
Peter Popov <p.dim.popov@gmail.com>
2022-10-30 14:10:58 +0200

    add serialize, parse and new methods to point

--- app/models/point/index.spec.ts
+++ app/models/point/index.spec.ts
@@ -1,20 +1,46 @@
-import type { Point, MaybePoint } from '~/models/point/index';
+import type { MaybePoint } from '~/models/point/index';
 import { PointOps } from '~/models/point/index';
 
 describe('Point', () => {
-  const sample: Point = { x: 1, y: 2 };
+  const sample = PointOps.new(1, 2);
 
-  it.each([
-    [sample, sample, true],
-    [sample, { ...sample }, true],
-    [{ x: 1, y: 2 }, { x: 1, y: 0 }, false],
-    [null, { x: 1, y: 2 }, false],
-    [{ x: 1, y: 2 }, null, false],
-    [null, null, true],
-  ])(
-    'should compare correctly - (%j), (%j)',
-    (point1: MaybePoint, point2: MaybePoint, areEqual: boolean) => {
-      expect(PointOps.equals(point1)(point2)).toEqual(areEqual);
-    },
-  );
+  describe(PointOps.new.name, () => {
+    it('should create point', () => {
+      const point = PointOps.new(1, 2);
+
+      expect(point).toEqual({ x: 1, y: 2 });
+    });
+  });
+
+  describe(PointOps.equals.name, () => {
+    it.each([
+      [sample, sample, true],
+      [sample, { ...sample }, true],
+      [{ x: 1, y: 2 }, { x: 1, y: 0 }, false],
+      [null, { x: 1, y: 2 }, false],
+      [{ x: 1, y: 2 }, null, false],
+      [null, null, true],
+    ])(
+      'should compare correctly - (%j), (%j)',
+      (point1: MaybePoint, point2: MaybePoint, areEqual: boolean) => {
+        expect(PointOps.equals(point1)(point2)).toEqual(areEqual);
+      },
+    );
+  });
+
+  describe(PointOps.serialize.name, () => {
+    it('should join x and y by comma', () => {
+      const result = PointOps.serialize(sample);
+
+      expect(result).toEqual('1,2');
+    });
+  });
+
+  describe(PointOps.parse.name, () => {
+    it('should work as expected', () => {
+      const result = PointOps.parse('1,2');
+
+      expect(result).toEqual(PointOps.new(1, 2));
+    });
+  });
 });
--- app/models/point/index.ts
+++ app/models/point/index.ts
@@ -6,7 +6,14 @@ export interface Point {
 export type MaybePoint = Point | undefined | null;
 
 export const PointOps = {
+  new: (x: number, y: number) => ({ x, y }),
   equals: (left: MaybePoint) => (right: MaybePoint) =>
     left === right ||
     (!!(left && right) && left.x === right.x && left.y === right.y),
+  serialize: (self: Point) => `${self.x},${self.y}`,
+  parse: (serialized: string): Point => {
+    const [x, y] = serialized.split(',').map(Number);
+
+    return { x, y };
+  },
 };
